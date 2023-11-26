8c73e33528cc13a7634794d92e942e2264cfa9fb
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-11-26 12:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//your JS code here. If required.
+
+const output = document.getElementById("output")
+
+let arr = [1, 2, 3, 4]
+function mapipulation(){
+	return new Promise((resolve) =>{
+		setTimeout(()=>{
+				resolve(arr)
+		},3000)
+	})
+}
+
+function filterOdd(array){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+				const filteredArr = array.filter(num=>num % 2 ===0)
+			resolve(filteredArr)
+		},1000)
+		
+	})
+}
+
+function Multiply(array){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+				const filteredArr = array.map(num=>num*2)
+			resolve(filteredArr)
+		},2000)
+		
+	})
+	
+}
+
+
+mapipulation(arr).then((result)=> filterOdd(result))
+ .then((ans)=> Multiply(ans)).then((result)=>{
+   output.innerText  = result
+ })
+
+
